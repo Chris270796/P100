319c7427dec823571fcff91059cff4fd71a05665
Chris270796 <84269447+Chris270796@users.noreply.github.com>
2022-04-21 09:28:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,51 +1,86 @@
-const display = document.getElementById('clock');
-const audio = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-alarm-digital-clock-beep-989.mp3');
-audio.loop = true;
-let alarmTime = null;
-let alarmTimeout = null;
+var SpeechRecognition = window.webkitSpeechRecognition;
 
-function updateTime() {
-    const date = new Date();
+var recognition = new SpeechRecognition();
 
-    const hour = formatTime(date.getHours());
-    const minutes = formatTime(date.getMinutes());
-    const seconds = formatTime(date.getSeconds());
-
-
-
-    display.innerText = `${hour} : ${minutes} : ${seconds}`
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
 }
 
-function formatTime(time) {
-    if (time < 10) {
-        return '0' + time;
+recognition.onresult = function(event) {
+
+    console.log(event);
+
+    var Content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    if (Content == "take my selfie") {
+        console.log("taking selfie");
+        speak();
     }
-    return time;
 }
 
-function setAlarmTime(value) {
-    alarmTime = value;
+
+function speak() {
+    var synth = window.speechSynthesis;
+
+    speak_data = "Taking your Selfie in 5 seconds ";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+    setTimeout(function() {
+        img_id = "selfie1";
+        speak_data = "Taking your Selfie in 5 seconds ";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+        take_snapshot();
+        save();
+    }, 5000);
 }
 
-function setAlarm() {
-    if (alarmTime) {
-        const current = new Date();
-        const timeToAlarm = new Date(alarmTime);
 
-        if (timeToAlarm > current) {
-            const timeout = timeToAlarm.getTime() - current.getTime();
-            alarmTimeout = setTimeout(() => audio.play(), timeout);
-            alert('Alarm set');
+camera = document.getElementById("camera");
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+});
+
+function take_snapshot() {
+    console.log(img_id);
+
+    Webcam.snap(function(data_uri) {
+        if (img_id == "photo1") {
+            document.getElementById(result1).innerHTML = '<img id="photo1" src="' + data_uri + '"/>';
         }
-    }
+        if (img_id == "photo2") {
+            document.getElementById(result1).innerHTML = '<img id="photo2" src="' + data_uri + '"/>';
+        }
+        if (img_id == "photo3") {
+            document.getElementById(result1).innerHTML = '<img id="photo3" src="' + data_uri + '"/>';
+        }
+        document.getElementById("result").innerHTML = '<img id="photo1" src="' + data_uri + '"/>';
+    });
 }
 
-function clearAlarm() {
-    audio.pause();
-    if (alarmTimeout) {
-        clearTimeout(alarmTimeout);
-        alert('Alarm cleared');
-    }
-}
 
-setInterval(updateTime, 1000);
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
